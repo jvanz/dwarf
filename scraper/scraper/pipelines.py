5350da086f1a0537dae6c67bb34d11e224febbb9
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-09-01 01:10:55 -0300

    scraper: use gRPC to send data to graph builder
    Adds a scrapy pipeline to send the links found within a web page to the
    graph builder service using gRPC. Thus the graph builder can build the
    web graph using those links between different domains.
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -5,7 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import grpc
 
-class ScraperPipeline(object):
+import graph_builder_pb2 as gb
+import graph_builder_pb2_grpc as gb_grpc
+
+
+class SendToGraphBuilder(object):
+    """ The link found to the graph builder """
+
+    def __init__(self):
+        self.channel = grpc.insecure_channel("localhost:50051")
+        self.stub = gb_grpc.GraphBuilderStub(self.channel)
+
     def process_item(self, item, spider):
+        response = self.stub.AddEdge(gb.Edge(source=item["source"], destination=item["destination"]))
+        print(response.status)
         return item
